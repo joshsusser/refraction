11061cd5955459578a048c38a62354d1f3333a87
Josh Susser & Wai Lun Mang <pair+jsusser+mang@pivotallabs.com>
2009-10-23 17:10:42 -0700

    helpful plugin install script

--- install.rb
+++ install.rb
@@ -1,3 +1,21 @@
-# TODO: generate refraction_rules.rb
+# generate refraction_rules.rb
 
-# puts "ADD TO ENVIRONMENT"
+  init_dir = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "config", "initializers"))
+  if test(?d, init_dir)
+    rules_file = File.join(init_dir, "refraction_rules.rb")
+    unless File.exists?(rules_file)
+      File.open(rules_file, "w") do |f|
+      f.puts(<<'EOF')
+Refraction.configure do |req|
+  # req.permanent! "http://example.com/"
+end
+EOF
+puts "Generated starter rules file in #{rules_file}"
+      end
+    end
+  end
+
+puts ""
+puts "Make sure to add Refraction to your middleware stack in your production environment:"
+puts '  config.middleware.insert_before(::Rack::Lock, ::Refraction, {})'
+puts ""
